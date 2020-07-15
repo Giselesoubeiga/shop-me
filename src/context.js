6a5a4005c36713aa00e343efac00ea1a8fd5b4fb
Gisele <giskey11@gmail.com>
2020-07-14 22:15:44 -0500

    cart columns added

--- src/context.js
+++ src/context.js
@@ -10,8 +10,11 @@ class ProductProvider extends Component {
     products: [],
     detailProduct: detailProduct,
     cart: [],
-    modalOpen: true,
+    modalOpen: false,
     modalProduct: detailProduct,
+    cartSubTotal:0,
+    cartTaxt:0,
+    cartTotal:0,
   };
   componentDidMount() {
     this.setProducts();
@@ -65,8 +68,26 @@ class ProductProvider extends Component {
   }
   closeModal = () =>{
     this.setState(()=>{
-      return {modalOpen:false}
-    })
+      return {modalOpen: false}
+    });
+  };
+
+  increment = (id) =>{
+    console.log('this is increment method');
+  }
+
+  decrement = (id) =>{
+    console.log('this is decrement method');
+  }
+
+  removeItem =(id)=>{
+    console.log("item removed");
+
+  }
+
+  clearCart =()=>{
+    console.log("cart was cleared");
+    
   }
   render() {
     return (
@@ -76,7 +97,11 @@ class ProductProvider extends Component {
           handleDetail: this.handleDetail,
           addToCart: this.addToCart,
           openModal:this.openModal,
-          closeModal:this.closeModal
+          closeModal:this.closeModal,
+          increment:this.increment,
+          decrement:this.decrement,
+          removeItem:this.removeItem,
+          clearCart:this.clearCart
         }}
       >
         {this.props.children}
